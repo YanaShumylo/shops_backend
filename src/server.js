5a6cd2774d7e8899689be6b7336addb0ae82cba7
ShumyloYana <zolotushko1@gmail.com>
2026-03-24 10:22:39 +0200

    Initial commit project shops

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+            target: 'pino-pretty',
+        },
+    }
+      ),
+  );
+
+  app.get('/', (req, res) => {
+  res.json({
+    message: 'Server is working',
+  });
+  });
+
+  app.use('/', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+  }
+  );
+};
+
